c78bc4bf60e276a8d331074386cc823a9c7395cb
Rahmattrik <rahmattrik@gmail.com>
2024-05-12 17:55:16 +0700

    feat: add forgot-password

---
+++ apps/web/src/hooks/api/auth/useForgotPassword.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { axiosInstance } from '@/lib/axios';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+interface ForgotPasswordResponse {
+    message: string,
+}
+
+const useForgotPassword = () => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    const forgotPassword = async (email: string) => {
+        try {
+            setIsLoading(true);
+            const { data } = await axiosInstance.post<ForgotPasswordResponse>(
+                "/auth/forgot-password", 
+                { email },
+            );
+            
+            alert(data.message);
+            router.replace('/');
+        } catch (error) {
+            //FIX ME: change alert to be toast
+            if (error instanceof AxiosError) {
+                alert(error.response?.data);
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return { 
+        forgotPassword, 
+        isLoading 
+    };
+};
+
+export default useForgotPassword;
